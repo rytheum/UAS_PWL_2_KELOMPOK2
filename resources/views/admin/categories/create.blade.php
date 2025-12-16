41edc48c9ee826df40af172fb7fb41b55c3f473d
AdityaSutanto <aditya.422024013@civitas.ukrida.ac.id>
2025-12-16 14:02:24 +0700

    Product Create, Edit, Fix Category Product

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('title', 'Tambah Category')
+@section('page-title', 'Tambah Category')
+
+@section('content')
+
+<div style="
+    background:#6676e4;
+    min-height:calc(100vh - 120px);
+    display:flex;
+    align-items:center;
+    justify-content:center;
+">
+
+    <div style="
+        background:white;
+        padding:30px;
+        border-radius:20px;
+        width:600px;
+    ">
+
+        <h2 style="margin-bottom:20px;text-align:center;">
+            Tambah Category
+        </h2>
+
+        <form action="{{ route('admin.categories.store') }}" method="POST">
+            @csrf
+
+            <div style="margin-bottom:20px;">
+                <label style="display:block;margin-bottom:8px;font-weight:600;">
+                    Category
+                </label>
+               <input type="text"
+                    name="category_name"
+                    placeholder="Nama Category"
+                    required
+                       style="
+                            width:100%;
+                            padding:12px;
+                            border-radius:8px;
+                            border:1px solid #ccc;
+                       ">
+            </div>
+
+            <div style="text-align:right;">
+                <button type="submit"
+                    style="
+                        background:#28a745;
+                        color:white;
+                        padding:10px 18px;
+                        border:none;
+                        border-radius:8px;
+                        cursor:pointer;
+                    ">
+                    Tambah +
+                </button>
+            </div>
+        </form>
+
+    </div>
+
+</div>
+
+@endsection
