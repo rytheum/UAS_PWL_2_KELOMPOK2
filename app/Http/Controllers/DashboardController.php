5b53c59f5862dcff58a54b8da21573c09642e942
AdityaSutanto <aditya.422024013@civitas.ukrida.ac.id>
2025-12-17 12:50:25 +0700

    Count All Resource di Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,63 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+// Import semua model yang dibutuhkan
+use App\Models\User;
+use App\Models\Product;
+use App\Models\ProductCategory;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
-    }
+        // Pastikan hanya admin yang bisa masuk
+        abort_if(auth()->user()->role !== 'admin', 403);
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+        // Hitung jumlah data
+        $totalCategories = ProductCategory::count();
+        $totalProducts = Product::count();
+        $totalTransactions = Transaction::count();
+        $totalUsers = User::count();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        // Kirim data ke view menggunakan compact()
+        return view('admin.dashboard', compact(
+            'totalCategories', 
+            'totalProducts', 
+            'totalTransactions', 
+            'totalUsers',
+        ));
     }
 }
