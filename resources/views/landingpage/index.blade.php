92631f5d0271706fbb09e7ce8b96efe75b2885c0
Ryth00 <josevan47106@gmail.com>
2025-12-18 12:30:11 +0700

    fix dikit

--- resources/views/landingpage/index.blade.php
+++ resources/views/landingpage/index.blade.php
@@ -270,7 +270,7 @@
                                 <li class="scroll"><a href="#feature">features</a></li>
                                 <li class="scroll"><a href="#blog">Blog</a></li>
                                 <li class="scroll"><a href="#newsletter">contact</a></li>
-                                <li class="transaction"><a href="{{ route('transactions.index') }}">Transaction</a>
+                                <li class="transaction"><a href="">Transaction</a>
                                 </li>
                             </ul><!--/.nav -->
                         </div><!-- /.navbar-collapse -->
